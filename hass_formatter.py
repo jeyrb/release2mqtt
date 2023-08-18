9741ed96df4a9bad7863d484dd84b501ade0a41e
jey burrows <jrb@jeymail.net>
2023-08-18 16:51:46 +0000

    fix mqtt discovery

--- hass_formatter.py
+++ hass_formatter.py
@@ -5,14 +5,16 @@ def hass_format_config(discovery,object_id,node_name,state_topic,command_topic,s
     if discovery.release_url:
         features.append('RELEASE_NOTES')
     config= {
-        'name':'%s %s' % (discovery.name,discovery.source_type),
+        'name':'%s %s on %s' % (discovery.name,discovery.source_type, node_name),
         'device_class':None, # not firmware, so defaults to null
         'unique_id':object_id,
         'state_topic':state_topic,
         'command_topic':command_topic,
-        'payload_install':{'source_type':discovery.source_type,'name':discovery.name,'command':'install'},
+        'payload_install':'{"source_type":"%s","name":"%s","command":"install"}' % (discovery.source_type,discovery.name),
         'source_session':session,
         'supported_features':features,
+        'entity_picture'    : discovery.entity_picture_url,
+        'icon'              : discovery.device_icon,
         'latest_version_topic':state_topic,
         'latest_version_template':'{{value_json.latest_version}}',
     } 
@@ -27,8 +29,6 @@ def hass_format_state(discovery,node_name,session):
         'title'             : discovery.title_template.format(name=discovery.name,node=node_name),
         'release_url'       : discovery.release_url,
         'release_summary'   : discovery.release_summary,
-        'entity_picture'    : discovery.entity_picture_url,
-        'icon'              : discovery.device_icon,
         'source_session'    : session
     } 
     state.update(discovery.provider.hass_state_format(discovery))
